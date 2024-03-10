bd69c6a83c0d511da9485c39d22aebcba0242e0a
Bulat Sh. <t.me/drakeninc>
2024-03-10 14:12:39 +0500

    v0.1.3 - Fix documentation errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,5 @@
 //! This crate adds the some useful macros for easily work
 
-pub use display_derive::Display;
-
-pub mod log;
-pub mod elog;
-pub mod str;
-pub mod re;
-pub mod map;
-pub mod set;
-
-
 /// Derive the [std::error::Error] trait (from crate [thiserror])
 /// 
 /// # Examples
@@ -25,3 +15,39 @@ pub mod set;
 /// }
 /// ```
 pub use thiserror::Error;
+
+/// Derive the [std::fmt::Display] trait
+/// 
+/// # Examples
+/// ```
+/// use add_macro::Display;
+/// 
+/// #[derive(Display)]
+/// struct Person {
+///     pub name: String,
+///     pub age: u8,
+/// }
+/// 
+/// impl Person {
+///     // !This method needs to formatting Display
+///     pub fn to_string(&self) -> String {
+///         format!("Hello, {}. Your age is {} years.)", &self.name, &self.age)
+///     }
+/// }
+/// 
+/// fn main() {
+///     let tomas = Person {
+///         name: "Tomas".to_owned(),
+///         age: 25,
+///     };
+///     println!("{tomas}");    // => Hello, Tomas. Your age is 25 years.
+/// }
+/// ```
+pub use display_derive::Display;
+
+pub mod log;
+pub mod elog;
+pub mod str;
+pub mod re;
+pub mod map;
+pub mod set;
